527c32890e05ab033d87e0af92b9999088f8024d
Bruno Dulcetti <brunodulcetti@gmail.com>
2017-01-04 19:13:47 -0200

    arquivo main.js para testes

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const a = 3;
+const b = 'asdasdasdasdasdasdasdasdasdsa asdasdasdasdasdasdasdasdasdsa asdasdasdasdasdasdasdasdasdsa asdasdasdasdasdasdasdasdasdsa asdasdasdasdasdasdasdasdasdsa asdasdasdasdasdasdasdasdasdsa asdasdasdasdasdasdasdasdasdsa';
+
+function teste() {
+    return a + b;
+}
+
+teste();
